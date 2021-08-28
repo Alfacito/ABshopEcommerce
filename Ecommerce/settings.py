5ac700c3f65962f6d4ad0c448c502070f28de00d
Bhawan sharma <83630093+Alfacito@users.noreply.github.com>
2021-08-28 17:16:51 +0530

    Commit

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,10 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS=[STATIC_DIR,]
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #Payment gateway
 RAZORPAY_API_KEY='rzp_test_lMbW52HiYUESqz'
