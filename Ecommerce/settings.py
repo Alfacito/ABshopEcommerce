a5a0d362f1200d3cd0d44bff7eb61a2a5d053507
Bhawan sharma <83630093+Alfacito@users.noreply.github.com>
2021-08-28 19:01:55 +0530

    Commit

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +27,7 @@ SECRET_KEY = 'kybjv#o$bcxupidrxp2sz)w#)g+6uu@5l=j58cs-52awetp&qn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','ab-shopping-kart.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -122,11 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'staticfiles')]
 
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #Payment gateway
 RAZORPAY_API_KEY='rzp_test_lMbW52HiYUESqz'
